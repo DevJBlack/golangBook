c6a4c9b022058e68df2054585d17c7450b28b7c0
Devin Black <Devinjamesblack@gmail.com>
2020-07-22 21:15:00 -0600

    making changes to the example folder

--- chapterTwo/example/main.go
+++ chapterTwo/example/main.go
@@ -6,20 +6,21 @@ import (
 	"golangBook/chapterTwo/tempconv"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	// scanner := bufio.NewScanner(os.Stdin)
+	oldScanner := bufio.NewScanner(os.Stdin)
 
-	// for scanner.Scan() {
-	// 	ucl := strings.ToLower(scanner.Text())
-	// 	fmt.Println(ucl)
-	// }
+	for oldScanner.Scan() {
+		ucl := strings.ToLower(oldScanner.Text())
+		fmt.Println(ucl)
+	}
 
-	// if err := scanner.Err(); err != nil {
-	// 	fmt.Fprintln(os.Stderr, "error:", err)
-	// 	os.Exit(1)
-	// }
+	if err := oldScanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
 
 	scanner := bufio.NewScanner(os.Stdin)
 
